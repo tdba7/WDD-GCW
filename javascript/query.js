6a740acd8d6020b7f221a5d5b321854f4a601dd1
5hehanthi <shehanthi2001@gmail.com>
2022-03-29 02:53:08 +0530

    changes

--- javascript/query.js
+++ javascript/query.js
@@ -3,27 +3,31 @@ function query(){
     var Aname = document.getElementById("name");
     var Asurname= document.getElementById("surname");
     var Aaddress = document.getElementById("address");
-    var Amail = document.getElementById("mail");
     var Aquery = document.getElementById("details");
+    var validateEmail=document.myform.mail.value;
+        var at=validateEmail.lastIndexOf("@");
+        var dot=validateEmail.lastIndexOf(".");
 
     if (Aname.value.length == 0){
         alert("Name Required!");
     }
+
     if (Asurname.value.length == 0){
         alert("Surname Required!");
     }
 
     if (Aaddress.value.length == 0){
         alert("Address Required!");
-    }
+    }   
 
-    if (Amail.value.length == 0){
-        alert("Email Required!");
+    if (at<1 || dot<at+2 || dot+2>=validateEmail.length){
+        alert("Please enter a Valid e-mail address");
     }
 
     if (Aquery.value.lenth == 0){
         alert("Query details are required!")
     }
+
     else{
         hideform();
     }
